795235c79108f1dfe400969eb5c04ccca4be90ce
Strikerg13 <strikerg13@strikerg13-Virtual-Machine.(none)>
2015-03-03 06:23:49 -0500

    Added fun.c

---
+++ fun.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "fun_lib.h"
+
+
+
+int main()
+{
+  //int *my_nums;
+  int user_choice = 0;
+  char rerun = ' ';
+  
+  do 
+  {
+    
+    // display the menu & get the user's option.
+    print_menu();
+    scanf("%d", &user_choice);
+  
+    // Run a process depending on what the user picks.
+    rerun = process_options(user_choice);
+  
+  }while (rerun == 'y');
+
+  
+  
+  printf("\n Bye Bye... \n");
+  
+
+  return 0;
+}
+
+
+
+
+
+
+
